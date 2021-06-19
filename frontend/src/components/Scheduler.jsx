056ac5293dee09ef9b3d8eccfee62b73c380fa9b
MihalachiBogdanMarian <b_mihalachi@yahoo.com>
2021-06-19 15:12:31 +0300

    urgency commit

--- frontend/src/components/Scheduler.jsx
+++ frontend/src/components/Scheduler.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Scheduler = () => {
+  const loggedInUser = useSelector((state) => state.loggedInUser);
+  const { userDetails } = loggedInUser;
+
+  const events = userDetails.schedules.map((schedule) => {
+    return {
+      id: schedule._id,
+      title: schedule.title + " - " + schedule.executionAddress,
+      start: schedule.availabilityPeriod.startTime,
+      end: schedule.availabilityPeriod.endTime,
+    };
+  });
+
   return (
     <>
       <h1>Your Scheduler</h1>
+      <hr></hr>
+      <br></br>
+      <div className="scheduler-container"></div>
     </>
   );
 };
